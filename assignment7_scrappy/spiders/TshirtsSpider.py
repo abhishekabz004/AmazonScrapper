9421b8b2974e4924d988aa341b44d18b123dd127
Abhishek N <abhishekabz004@gmail.com>
2018-04-03 20:43:14 +0530

    simplification

--- assignment7_scrappy/spiders/TshirtsSpider.py
+++ assignment7_scrappy/spiders/TshirtsSpider.py
@@ -21,22 +21,18 @@ class TshirtsspiderSpider(scrapy.Spider):
     start_urls = ['https://www.amazon.in/T-Shirts-Polos-Men/s?ie=UTF8&page=1&rh=n%3A1968120031']
     #rules = [Rule(LinkExtractor(allow=['https://images-eu.ssl-images-amazon.com/images/*']),'parse')]
     def parse(self, response):
-    	category = response.css('div.searchUndoAUIHacks')[0]
-    	category = response.css('div.a-row div.a-span8 div.s-first-column div.a-spacing-top-small span span span.a-color-state::text').extract_first()#xpath('//div[@id="search-main-wrapper"]//div[@class="nav_redesign s-left-nav-rib-redesign s-span-page"]//div[@class="searchTemplate imageLayout so_in_e"]');
-    	
+    	category = response.css('span.a-color-state.a-text-bold::text').extract_first()
     	for product in response.css('li.s-result-item'):
-    		product = product.css('div.s-item-container')
-    		cost = product.css('div.a-spacing-none')[2]
-    		image_url = product.css('div.a-spacing-base div.a-span12 span.a-declarative div.a-gesture-horizontal div.s-active a.a-link-normal img.s-access-image::attr(src)').extract_first()#.encode('utf-8')
+    		image_url = product.css('img.s-access-image.cfMarker::attr(src)').extract_first()
     		yield Assignment7ScrappyItem(image_urls=[image_url])
     		#cost = product.xpath('//div[@class="a-row a-spacing-none"]/div[@class="a-row a-spacing-none"]//a[@class="a-link-normal a-text-normal"]')
     		yield {
     			'Id': staticNum(),
-    			'Name': product.css('div.a-spacing-none div.a-spacing-mini a.s-color-twister-title-link h2.s-access-title::text').extract_first(),
-    			'imageUrl': product.css('div.a-spacing-base div.a-span12 span.a-declarative div.a-gesture-horizontal div.s-active a.a-link-normal img.s-access-image::attr(src)').extract_first(),                
-    			'ProductUrl': product.css('div.a-spacing-none div.a-spacing-mini a.s-color-twister-title-link::attr(href)').extract_first(),
-    			'Cost': cost.css('a.a-link-normal span.s-price::text').extract_first(),
-    			'Review': product.css('div.a-spacing-none div.a-spacing-top-mini span span.a-declarative a.a-popover-trigger i.a-icon span.a-icon-alt::text').extract_first(),
+    			'Name': product.css('h2.a-size-base.s-inline.s-access-title.a-text-normal::text').extract_first(),
+    			'imageUrl': image_url,
+    			'ProductUrl': product.css('a.a-link-normal.s-access-detail-page.s-color-twister-title-link.a-text-normal::attr(href)').extract_first(),
+    			'Review': product.css('div.s-item-container div.a-spacing-none div.a-spacing-top-mini span span.a-declarative a.a-popover-trigger i.a-icon span.a-icon-alt::text').extract_first(),
+    			'Cost': product.css('span.a-size-base.a-color-price.s-price.a-text-bold::text').extract_first(),
     			'Category': category,
     		}
     	next_page = response.css('span.pagnLink')[1]
